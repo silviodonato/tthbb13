9415ff2c4a38280e5b3667f39539a6218e7483e1
Silvio Donato <silvio.donato@cern.ch>
2016-07-25 15:18:34 +0200

    nothing2

--- MEAnalysis/gc/projectSkim.sh
+++ MEAnalysis/gc/projectSkim.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source common.sh
-#export DATASETPATH=ttHTobb_M125_13TeV_powheg_pythia8
-#export FILE_NAMES=/store/user/jpata/tth/Jun17_leptonic_nome/TT_TuneEE5C_13TeV-powheg-herwigpp/Jun17_leptonic_nome/160617_165529/0000/tree_1.root
-#export GC_SCRATCH=/scratch/jpata
+#export DATASETPATH=ttHTobb_M125_13TeV_powheg_pythia8_dsalerno-VHBBHeppyV21_tthbbV9_v3_3
+#export FILE_NAMES=/store/user/dsalerno/tth/VHBBHeppyV21_tthbbV9_v3_3/ttHTobb_M125_13TeV_powheg_pythia8/VHBBHeppyV21_tthbbV9_v3_3/160518_110721/0000/tree_10.root = 1634
+#export GC_SCRATCH=/scratch/sdonato
 
 #go to work directory
 cd $GC_SCRATCH
