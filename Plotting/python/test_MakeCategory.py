737a5adf3a3d79f4b5b5deb2b01aa2e19e50959f
Silvio Donato <silvio.donato@cern.ch>
2016-08-17 10:13:23 +0200

    use a separate function to run the test_makeCategory for FH

--- Plotting/python/test_MakeCategory.py
+++ Plotting/python/test_MakeCategory.py
@@ -6,17 +6,14 @@ import ROOT
 
 class MakeCategoryTestCase(unittest.TestCase):
     
-    def test_MakeCategory_run(self):
+    def launch_MakeCategory_test(self,catname,analysis, var):
         from TTH.Plotting.Datacards.MakeCategory import main as MakeCategory_main
-        catname = "sl_jge6_tge4"
-        #catname = "fh_j8_t4" #(for FH)
         MakeCategory_main(
             os.path.join(
                 os.environ["CMSSW_BASE"],
                 "src/TTH/Plotting/python/Datacards/AnalysisSpecification.py",
             ),
-            "SL_7cat",
-            #"FH", #(for FH)
+            analysis,
             catname
         )
         outfile = "{0}.root".format(catname)
@@ -26,12 +23,18 @@ class MakeCategoryTestCase(unittest.TestCase):
         self.assertFalse(fi == None)
 
         for samp in ["ttH_hbb", "data", "data_obs"]:
-            hname = "{0}/{1}/jetsByPt_0_pt".format(samp, catname)
+            hname = "{0}/{1}/{2}".format(samp, catname, var)
             logging.debug("test_MakeCategory_run: getting {0}".format(hname))
             h = fi.Get(hname)
             logging.debug("test_MakeCategory_run: {0} integral={1}".format(hname, h.Integral()))
             self.assertFalse(h == None)
             self.assertTrue(h.Integral() > 0.0)
+    
+    def test_MakeCategory_run_SL(self):
+        self.launch_MakeCategory_test("sl_jge6_tge4","SL_7cat","jetsByPt_0_pt")
+
+#    def test_MakeCategory_run_FH(self):
+#        self.launch_MakeCategory_test("fh_j7_t3","FH","btag_LR_4b_2b_btagCMVA_logit")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
